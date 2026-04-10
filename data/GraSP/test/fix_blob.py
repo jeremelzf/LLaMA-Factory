ab030807424c946bcdde191a2bf21fe284f583cf
Jereme Lee <jeremelzf@gmail.com>
2026-04-10 14:25:57 +0800

    Script to fix blob

--- data/GraSP/test/fix_blob.py
+++ data/GraSP/test/fix_blob.py
@@ -1,7 +1,7 @@
 import json
 
 input_path = "/nfs/home/svu/e0957602/LLaMA-Factory/data/GraSP/test/GraSP_test_blob.jsonl"
-output_path = "/nfs/home/svu/e0957602/LLaMA-Factory/data/GraSP/test/GraSP_test_fixed.jsonl"
+output_path = "/nfs/home/svu/e0957602/LLaMA-Factory/data/GraSP/test/GraSP_test.jsonl"
 
 with open(input_path, "r") as f:
     content = f.read()
